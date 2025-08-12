07fcb1d62d6877cedef9ea0e6893f46eed6a57b6
Didarul Alam Rahat <darahat42@gmail.com>
2025-08-12 20:25:16 +0600

    Fix: Router problem fixed. in navigation bar now only home and utouchat and aichat. other options moved to drawer

--- lib/features/home/presentation/widgets/home_bottom_nav.dart
+++ lib/features/home/presentation/widgets/home_bottom_nav.dart
@@ -2,6 +2,7 @@ import 'package:ai_chat/features/home/provider/home_provider.dart';
 import 'package:ai_chat/l10n/app_localizations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 /// HomeBottomNav widget class
 class HomeBottomNav extends ConsumerWidget {
@@ -15,22 +16,35 @@ class HomeBottomNav extends ConsumerWidget {
       currentIndex: currentIndex,
       onTap: (index) {
         ref.read(homeControllerProvider.notifier).changeTab(index);
+        // Navigate using GoRouter
+        switch (index) {
+          case 0:
+            context.go('/home');
+            break;
+          case 1:
+            context.go('/uToUChat');
+            break;
+          case 2:
+            context.go('/aiChat');
+            break;
+        }
       },
       items: [
         BottomNavigationBarItem(
           icon: const Icon(Icons.home),
           label: AppLocalizations.of(context)!.home,
         ),
-        BottomNavigationBarItem(
-          icon: const Icon(Icons.search),
-          label: AppLocalizations.of(context)!.search,
-        ),
-        BottomNavigationBarItem(
-          icon: const Icon(Icons.settings),
-          label: AppLocalizations.of(context)!.settings,
-        ),
+
+        // BottomNavigationBarItem(
+        //   icon: const Icon(Icons.search),
+        //   label: AppLocalizations.of(context)!.search,
+        // ),
         BottomNavigationBarItem(
           icon: const Icon(Icons.chat_bubble),
+          label: AppLocalizations.of(context)!.aiChat,
+        ),
+        BottomNavigationBarItem(
+          icon: const Icon(Icons.chat),
           label: AppLocalizations.of(context)!.aiChat,
         ),
       ],
