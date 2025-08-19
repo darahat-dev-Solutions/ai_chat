5868e5beaa2000c7796cf60127f7e88af24eea66
Didarul Alam Rahat <darahat42@gmail.com>
2025-08-19 09:28:42 +0600

    Fix: Forground and background messeging is comming from firebase console. but message notification not working when new message arrived

--- lib/core/services/initialization_service.dart
+++ lib/core/services/initialization_service.dart
@@ -1,24 +1,30 @@
-import 'package:ai_chat/core/services/firebase_messaging_service.dart';
 import 'package:ai_chat/core/services/hive_service.dart';
 import 'package:ai_chat/firebase_options.dart';
 import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+/// Initialization Service class
+/// where all kind of Initialization takes places
+
 class InitializationService {
   final Ref _ref;
+
+  /// Initialization Service Constructor
   InitializationService(this._ref);
 
+  /// Main Initialize function
   Future<void> initialize() async {
-    WidgetsFlutterBinding.ensureInitialized();
+    /// Ensure Flutter binding is initialized
+
+    /// Register background message handler
     await dotenv.load(fileName: ".env");
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
     );
+
     await FirebaseAppCheck.instance.activate(
       androidProvider: AndroidProvider.debug,
       appleProvider: AppleProvider.appAttest,
@@ -30,14 +36,13 @@ class InitializationService {
     const InitializationSettings initSettings = InitializationSettings(
       android: androidSettings,
     );
-    await flutterLocalNotificationsPlugin.initialize(initSettings);
-    final messagingService = _ref.read(firebaseMessagingServiceProvider);
-    await messagingService.initialize();
+    // await flutterLocalNotificationsPlugin.initialize(initSettings);
 
-    FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
+    // FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
   }
 }
 
+/// initializationServiceProvider provider variable
 final initializationServiceProvider = Provider<InitializationService>(
   (ref) => InitializationService(ref),
 );
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,19 @@
 import 'package:ai_chat/app.dart';
+import 'package:ai_chat/core/services/firebase_messaging_service.dart';
 import 'package:ai_chat/core/services/initialization_service.dart';
 import 'package:ai_chat/core/utils/logger.dart';
 import 'package:ai_chat/features/auth/provider/auth_providers.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 /// Top Level function to handle background messages
 
 Future<void> main() async {
-  // WidgetsFlutterBinding.ensureInitialized();
-  AppLogger.init();
+  WidgetsFlutterBinding.ensureInitialized(); // Moved here
+  AppLogger.init(); // Moved here
+  // Moved here
   final container = ProviderContainer();
-  // await dotenv.load(fileName: ".env");
-  // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
-  // await FirebaseAppCheck.instance.activate(
-  //   androidProvider: AndroidProvider.debug,
-  //   appleProvider: AppleProvider.appAttest,
-  // );
 
   try {
     await container.read(initializationServiceProvider).initialize();
@@ -31,13 +27,6 @@ Future<void> main() async {
   }
 
   container.read(authControllerProvider.notifier).checkInitialAuthState();
+  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
   runApp(UncontrolledProviderScope(container: container, child: const App()));
-  // final firebaseMessagingService = container.read(
-  //   firebaseMessagingServiceProvider,
-  // );
-  // await firebaseMessagingService.initialize();
-  // FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-  // container.read(authControllerProvider.notifier).checkInitialAuthState();
-
-  // runApp(UncontrolledProviderScope(container: container, child: const App()));
 }
