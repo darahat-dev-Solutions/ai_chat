099613cc6b775f8b4db911a493afcacefede732b
Didarul Alam Rahat <darahat42@gmail.com>
2025-09-14 20:29:40 +0600

    Feat: Firebase Local authentication completed

--- lib/core/services/initialization_service.dart
+++ lib/core/services/initialization_service.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:ai_chat/core/services/hive_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_app_check/firebase_app_check.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -21,11 +25,23 @@ class InitializationService {
     /// Register background message handler
     await dotenv.load(fileName: ".env");
     await Firebase.initializeApp();
+    if (dotenv.env['USE_FIREBASE_EMULATOR'] == 'true') {
+      // For Android emulator use 10.0.2.2. for iOS use localhost.
+      final host = Platform.isAndroid ? '10.0.2.2' : 'localhost';
+      await FirebaseAuth.instance.useAuthEmulator(host, 9099);
+      FirebaseFirestore.instance.useFirestoreEmulator(host, 8080);
+    } else {
+      //Only activate App Check when not using the emulator/cloud Firebase.
+      await FirebaseAppCheck.instance.activate(
+        androidProvider: AndroidProvider.debug,
+        appleProvider: AppleProvider.appAttest,
+      );
+    }
 
-    await FirebaseAppCheck.instance.activate(
-      androidProvider: AndroidProvider.debug,
-      appleProvider: AppleProvider.appAttest,
-    );
+    // await FirebaseAppCheck.instance.activate(
+    //   androidProvider: AndroidProvider.debug,
+    //   appleProvider: AppleProvider.appAttest,
+    // );
     await ref.read(hiveServiceProvider).init();
   }
 }
