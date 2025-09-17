0cb8b526b345bdc7226f5a796d69b415944a18c9
darahat <darahat42@gmail.com>
2025-09-17 16:29:29 +0600

    Fix: Issues fixed inside api service

--- lib/features/app_settings/infrastructure/settings_repository.dart
+++ lib/features/app_settings/infrastructure/settings_repository.dart
@@ -1,4 +1,7 @@
-import 'package:ai_chat/core/constants/hive_constants.dart';
+import 'package:ai_chat/core/services/hive_service.dart';
+import 'package:ai_chat/core/utils/logger.dart';
+import 'package:ai_chat/features/app_settings/domain/settings_model.dart';
+import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 /// Settings Repository class where all get and Save functions will exist
@@ -12,44 +15,74 @@ class SettingsRepository {
   static const String _defaultThemeMode = 'system';
   static const String _defaultLocale = 'en';
   static const int _defaultAiChatModuleId = 1;
+  final AppLogger appLogger = AppLogger();
+
+  final HiveService hiveService;
+  Box<SettingDefinitionModel> get _box => hiveService.settingsBox;
+
+  SettingsRepository(this.hiveService);
 
   /// Retrives the saved theme model
   /// Returns the default theme ('system') if no value is found.
   Future<String> getThemeMode() async {
-    final settingsBox = await Hive.openBox(HiveConstants.settingsBoxName);
-    return settingsBox.get(_themeModeKey, defaultValue: _defaultThemeMode);
+    // final settingsBox = await Hive.openBox(HiveConstants.settingsBoxName);
+    final SettingDefinitionModel? model = _box.get(_themeModeKey);
+    return model?.defaultValue ?? _defaultThemeMode;
   }
 
   /// Saves the selected theme mode.
   Future<void> saveThemeMode(String themeMode) async {
-    final settingsBox = await Hive.openBox(HiveConstants.settingsBoxName);
-    return settingsBox.put(_themeModeKey, themeMode);
+    // final settingsBox = await Hive.openBox(HiveConstants.settingsBoxName);
+    final SettingDefinitionModel model = SettingDefinitionModel(
+      id: 1,
+      name: _themeModeKey,
+      description: 'Application theme mode(light, dark, system)',
+      defaultValue: themeMode,
+      dataType: 'STRING',
+      isUserSpecific: false,
+    );
+    return _box.put(_themeModeKey, model);
   }
 
   /// Retrives the saved Locale
   /// Returns the default locale ('en') if no value is found
   Future<String> getLocale() async {
-    final settingsBox = await Hive.openBox(HiveConstants.settingsBoxName);
-    return settingsBox.get(_localKey, defaultValue: _defaultLocale);
+    final SettingDefinitionModel? model = _box.get(_localKey);
+    return model?.defaultValue ?? _defaultLocale;
   }
 
   Future<void> saveLocale(String locale) async {
-    final settingsBox = await Hive.openBox(HiveConstants.settingsBoxName);
-    return settingsBox.put(_localKey, locale);
+    // final settingsBox = await Hive.openBox(HiveConstants.settingsBoxName);
+    final SettingDefinitionModel model = SettingDefinitionModel(
+      id: 2,
+      name: _localKey,
+      description: 'Application language',
+      defaultValue: locale,
+      dataType: 'STRING',
+      isUserSpecific: false,
+    );
+    return _box.put(_localKey, model);
   }
 
   /// Retrives the saved AiChatModule
-  /// Returns the default chatModule ('GeneralAIAssistant') if no value is found
+  /// Returns the default chatModule id ('1') if no value is found
   Future<String> getAiChatModuleId() async {
-    final settingsBox = await Hive.openBox(HiveConstants.settingsBoxName);
-    return settingsBox.get(_aiChatModuleKey,
-        defaultValue: _defaultAiChatModuleId);
+    final SettingDefinitionModel? model = _box.get(_aiChatModuleKey);
+    return model?.defaultValue?.toString() ?? _defaultAiChatModuleId.toString();
   }
 
   /// Saves the selected AI Chat Module.
   Future<void> saveAiChatModuleId(int id) async {
-    final settingsBox = await Hive.openBox(HiveConstants.settingsBoxName);
-    return settingsBox.put(_aiChatModuleKey, id);
+    // final settingsBox = await Hive.openBox(HiveConstants.settingsBoxName);
+    final SettingDefinitionModel model = SettingDefinitionModel(
+      id: 3,
+      name: _aiChatModuleKey,
+      description: 'AI Chat Module ID',
+      defaultValue: id.toString(),
+      dataType: 'INT',
+      isUserSpecific: false,
+    );
+    return _box.put(_aiChatModuleKey, model);
   }
 
   // static final SettingDefinitionModel _themeModeSettingDefinition =
